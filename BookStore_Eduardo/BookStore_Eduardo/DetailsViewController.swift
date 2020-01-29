867af2183217ef4aaf14d2b315da0e7291aafd48
Eduardo Pinto <eduardo.pinto@itsector.pt>
2020-01-29 17:58:36 +0000

    + Add: Added project

---
+++ BookStore_Eduardo/BookStore_Eduardo/DetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailsViewController.swift
+//  BookStore_Eduardo
+//
+//  Created by itsector on 29/01/2020.
+//  Copyright © 2020 itsector. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ BookStore_Eduardo/BookStore_Eduardo/ListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ListViewController.swift
+//  BookStore_Eduardo
+//
+//  Created by itsector on 29/01/2020.
+//  Copyright © 2020 itsector. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
